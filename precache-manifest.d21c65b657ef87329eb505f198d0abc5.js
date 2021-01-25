9a1b4f1d858fe316bb2b5ce8511238c23447b510
hotspoon <fariez18@outlook.com>
2021-01-25 11:56:07 +0700

    Updates

---
+++ precache-manifest.d21c65b657ef87329eb505f198d0abc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b29b57303c8eb60e9f8e",
+    "url": "/portofolio-react/static/css/main.f08d5e42.chunk.css"
+  },
+  {
+    "revision": "b29b57303c8eb60e9f8e",
+    "url": "/portofolio-react/static/js/main.b29b5730.chunk.js"
+  },
+  {
+    "revision": "bbcd7879a2a60b7b6907",
+    "url": "/portofolio-react/static/js/runtime~main.bbcd7879.js"
+  },
+  {
+    "revision": "77e2a8e6f10b7a79c9ca",
+    "url": "/portofolio-react/static/js/2.77e2a8e6.chunk.js"
+  },
+  {
+    "revision": "ad0396172444151f94722e922163b957",
+    "url": "/portofolio-react/index.html"
+  }
+];
